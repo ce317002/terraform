663568f84102ff4e4cdc8ea781f384d9234bc5e0
wendy martin <wmartin0000@gmail.com>
2020-09-03 17:41:30 +0700

    modification file terraform.tf

--- terraforms.tf
+++ terraforms.tf
@@ -1,12 +1,20 @@
-provider "linode" {
-    token = "your-linode-api-token"
-}
+provider "google" {
+    project = "terraform-project"
+    name    = "terraform"
+    region  = "us-central1"
+    zone    = "us-central1-a"
 
-resource "linode_instance" "example_instance" {
-    label = "example_instance_label"
-    image = "linode/ubuntu18.04"
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance"
+    image = "ubuntu/ubuntu18.04"
     region = "us-central"
     type = "g6-standard-1"
-    authorized_keys = ["ssh-rsa AAAA...Gw== user@example.local"]
     root_pass = "your-root-password"
+
+    network_interface {
+        network =  "default"
+        access_config {
+        }
+    }
+
 }
