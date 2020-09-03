0ea4ae2b99f722753a5e3fdebb9a5c1ce7d9506a
wendy martin <wmartin0000@gmail.com>
2020-09-03 17:50:22 +0700

    modification file terraform.tf 2

--- terraforms.tf
+++ terraforms.tf
@@ -3,6 +3,7 @@ provider "google" {
     name    = "terraform"
     region  = "us-central1"
     zone    = "us-central1-a"
+}
 
 resource "google_compute_instance" "vm_instance" {
     name = "terraform-instance"
